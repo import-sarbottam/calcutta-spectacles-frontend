58955e3520e2034112b621e954150252f22171b7
Sarbottam Chatterjee <sarbottamchatterjee1775@gmail.com>
2023-01-31 22:00:59 +0530

    Version 0.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Basicinfo from "./components/Basicinfo";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={container}>
+      <Basicinfo/>
     </div>
   );
+}
+
+const container = {
+  backgroundColor: 'grey',
+  display:'flex',
+  // alignItems: 'center',
+  justifyContent: 'center',
+  minHeight: '100vh',
+  width: '100%',
 }
 
 export default App;
