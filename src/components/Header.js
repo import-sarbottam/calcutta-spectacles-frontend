17d2596dbfebcfda1288334960dbddce619615f6
Samrat Ganguly <tanmoydas407@gmail.com>
2023-02-13 23:20:31 +0530

    header

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import "../style/Header/main.css";
+const Head = () => {
+  return (
+    <div className="container">
+      <ul>
+        <li>
+          <label>Bill</label>
+        </li>
+        <li>
+          <label>Frame</label>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Head;
